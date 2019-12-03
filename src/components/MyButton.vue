f019c2d53d3665642c90da8105004d7d030a23fc
George Pipkin <gpp8p@Virginia.EDU>
2019-12-03 12:28:34 -0500

    refactored click handler.  Added MyButton

---
+++ src/components/MyButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <a href="#" class="myButton">{{ buttonLabel }}</a>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+  props: {
+    buttonLabel: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.myButton {
+  box-shadow: 0px 1px 0px 0px #f0f7fa;
+  background: linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
+  background-color: #33bdef;
+  border-radius: 6px;
+  border: 1px solid #057fd0;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 4px 24px;
+  text-decoration: none;
+  text-shadow: 0px -1px 0px #5b6178;
+}
+.myButton:hover {
+  background: linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
+  background-color: #019ad2;
+}
+.myButton:active {
+  position: relative;
+  top: 1px;
+}
+</style>
--- src/views/EditGrid1.vue
+++ src/views/EditGrid1.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h2 ref="prompt">Click On Top Left Cell</h2>
+        <span class="prompt" ref="prompt">Click On Top Left Cell<MyButton buttonLabel="Yes"></MyButton></span>
         <div class="softGridWrapper">
             <blank-component v-for="(instance, index) in cardInstances"
                              :key="index"
@@ -19,21 +19,29 @@
 
   import axios from 'axios';
   import blankComponent from "../components/blankComponent";
+  import MyButton from "../components/MyButton";
   export default {
     name: "EditGrid1",
-    components: { blankComponent },
+    components: { blankComponent, MyButton },
     data() {
       return {
         cardInstances: [],
         topLeftClicked: 0,
-        bottomRightClicked: 0
+        bottomRightClicked: 0,
+        cstatus: 0
       }
     },
     created: function() {
+      this.WAITINGFORCLICK=0;
+      this.TOPLEFTCLICKED =1;
+      this.BOTTOMRIGHTCLICKED=2;
+      this.WAITINGTOSAVE=3;
+      this.CANCELLAYOUTUPDFATE=4;
       axios.get(`http://localhost:8000/getLayout?layoutId=5`)
         .then(response => {
           // JSON responses are automatically parsed.
           this.cardInstances = response.data;
+          this.cstatus=this.WAITINGFORCLICK;
         })
         .catch(e => {
           this.errors.push(e);
@@ -42,19 +50,26 @@
     methods:{
       processClick(msg){
         console.log(msg[0]);
-        debugger;
-        if(this.topLeftClicked==0){
-          this.topLeftClicked=msg[0];
-        }else{
-          if(this.bottomRightClicked==0){
-            this.bottomRightClicked = msg[0];
-          }else{
+        switch(this.cstatus){
+          case this.WAITINGFORCLICK:
             this.topLeftClicked=msg[0];
-            this.bottomRightClicked=0;
-          }
-        }
-        this.$refs.prompt.innerHTML = "Click Bottom Right Cell";
+            this.cstatus=this.TOPLEFTCLICKED;
+            this.$refs.prompt.innerHTML = "Click Bottom Right Cell";
+            break;
+          case this.TOPLEFTCLICKED:
+            this.bottomRightClicked = msg[0];
+            this.cstatus=this.BOTTOMRIGHTCLICKED;
+            this.$refs.prompt.innerHTML = "Is this the area you wish to fill ? ";
+            break;
+          case this.BOTTOMRIGHTCLICKED:
+            this.$refs.prompt.innerHTML = "Start over - click on top left cell";
+            this.cstatus = this.WAITINGFORCLICK;
+            this.topLeftClicked=0;
+            this.bottomRightClicked
+            break;
 
+
+        }
       }
     }
   };
@@ -70,5 +85,12 @@
         height: 90vh;
         color: #444;
     }
+
+    .prompt {
+        font-size: medium;
+        color: blue;
+    }
+
+
 
 </style>
