9bcdfc5f4c68adf5218c63d75d8091b771c4b167
George Pipkin <gpp8p@Virginia.EDU>
2020-01-17 22:56:19 -0500

    config Headline Card in Nav Bar

--- src/components/HeadlineConfig.vue
+++ src/components/HeadlineConfig.vue
@@ -1,6 +1,25 @@
 <template>
     <span>
         <span v-if="this.statusNow==INPUT_BACKGROUND" class="layoutMenuItem">
+            <input type="radio" name="background_type" value="color" @click="setBackgroundColor" />Background Color
+            <input type="radio" name="background_type" value="image" @click="setBackgroundImage" />Background Image
+            <span v-if="backgroundInputStatus==ENTER_BACKGROUND_COLOR">
+            <select ref="backgroundColorSelect" @change="backgroundSelectionMade($event)">
+                <option value="select">Select Color</option>
+                <option  value="#000000">Black</option>
+                <option  value="#000080">Navy Blue</option>
+                <option  value="#0000CD">MediumBlue</option>
+                <option  value="#0000FF">Blue</option>
+                <option  value="#006400">DarkGreen</option>
+                <option  value="#008080">Teal</option>
+                <option  value="#008B8B">DarkCyan</option>
+                <option  value="#00BFFF">DeepSkyBlue</option>
+                <option  value="#00CED1">DarkTurquoise</option>
+                <option  value="#00FA9A">MediumSpringGreen</option>
+                <option  value="#00FF00">Lime</option>
+            </select>
+
+            </span>
             <MyButton @myButtonClicked="bumpStatus" buttonLabel="Next"></MyButton><MyButton @myButtonClicked="this.cancel" buttonLabel="Cancel"></MyButton>
         </span>
         <span v-if="this.statusNow==INPUT_TEXT_STYLE" class="layoutMenuItem">
@@ -27,7 +46,11 @@ import Vue from "vue";
         INPUT_TEXT_STYLE: 1,
         INPUT_TEXT: 2,
         SAVE_CONFIG: 3,
-        statusNow: this.INPUT_BACKGROUND
+        ENTER_BACKGROUND_COLOR: 0,
+        ENTER_BACKGROUND_IMAGE: 1,
+        ENTER_BACKGROUND_UNKNOWN:3,
+        statusNow: this.INPUT_BACKGROUND,
+        backgroundInputStatus: this.ENTER_BACKGROUND_COLOR
       }
     },
     methods: {
